7a11f87a3ef12e06c81ccc8afc5e3c3cf69496c9
JulioNunesDev <julionunes999@gmail.com>
2022-10-07 09:37:13 -0300

    feat: lib http-sts-codes

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,9 +1,11 @@
 import {Router} from 'express';
+import {StatusCodes} from 'http-status-codes';
+
 const router = Router();
 
 
 router.get('/', (req, res) => {
-  return res.send('Olá, DEV!');
+  return res.status(StatusCodes.OK).send('Olá, DEV!');
 });
 
 export {router};
