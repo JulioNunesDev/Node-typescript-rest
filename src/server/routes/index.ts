0d7de2bbc8f60b9cf0552ff5b059a1e1bc9cd08e
JulioNunesDev <julionunes999@gmail.com>
2022-10-11 23:31:49 -0300

    feat: add Controller city

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,11 +1,14 @@
 import {Router} from 'express';
 import {StatusCodes} from 'http-status-codes';
+import {CidadesController} from './../controllers';
 
 const router = Router();
 
-
+ 
 router.get('/', (req, res) => {
   return res.status(StatusCodes.OK).send('Olá, DEV!');
 });
+
+router.post('/cidades', CidadesController.create);
 
 export {router};
