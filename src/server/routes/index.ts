ce24542a6824da99bb78d4cc88df5baf6484ec77
JulioNunesDev <julionunes999@gmail.com>
2022-11-01 22:38:37 -0300

    feat: routes, delete, put

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -13,8 +13,20 @@ router.get('/cidades',
   CidadesController.getAllValidation,
   CidadesController.getAll);
 
+router.get('/cidades/:id',
+  CidadesController.getByIdValidation,
+  CidadesController.getById);
+
 router.post('/cidades',
   CidadesController.createValidator,
   CidadesController.create);
+
+router.put('/cidades/:id',
+  CidadesController.updateByIdValidation,
+  CidadesController.updateById);
+
+router.delete('/cidades/:id',
+  CidadesController.deleteByIdValidation,
+  CidadesController.deleteById);
 
 export {router};
